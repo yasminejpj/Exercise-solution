34e6b4ed1ea7a5d7f1320311e9b0795de6a6fc19
Yasmine <yasmine@gmail.com>
2022-07-05 19:36:01 +0100

    add question.1.js

---
+++ git.demo3/question.1.js
@@ -0,0 +1,9 @@
+// Question 1
+
+const teachingTeam = ['Leo', 'Kirti', 'Pj', 'Gemma', 'James', 'Peter'];
+
+for (member of teachingTeam) {
+    if (member.includes('o')) {
+        console.log(member);
+    }
+}
