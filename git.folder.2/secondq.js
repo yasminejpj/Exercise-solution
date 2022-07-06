01a54639b3a9f030b2cbadc510eef5540fb713ae
Yasmine <yasmine@gmail.com>
2022-07-06 14:55:14 +0100

    question 2 added.

---
+++ git.folder.2/secondq.js
@@ -0,0 +1,7 @@
+// Question 2
+
+const teachingTeam = ['Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P'];
+
+for (let i = 0; i < teachingTeam.length; i++) {
+    teachingTeam[i] = teachingTeam[i].toUpperCase();
+}
