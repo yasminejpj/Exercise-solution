17d566530b434627e4378e54f3718a04291437a6
Yasmine <yasmine@gmail.com>
2022-07-06 13:39:01 +0100

    Add Solution to Question 1 of the Exercise.
    This is a multi-line commit message for our test.;

---
+++ git.folder.2/firstq.js
@@ -0,0 +1,8 @@
+// Question 1
+const teachingTeam = ['Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P'];
+
+for (member of teachingTeam) {
+    if (member.includes('o')) {
+        console.log(member);
+    }
+}
