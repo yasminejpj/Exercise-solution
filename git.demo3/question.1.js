acb6a8ecc6e0c0f2cf1d43c6ffb45a072f2111d7
Yasmine <yasmine@gmail.com>
2022-07-07 10:39:31 +0100

    add three.1 using a single scan

--- git.demo3/question.1.js
+++
@@ -1,9 +0,0 @@
-// Question 1
-
-const teachingTeam = ['Leo', 'Kirti', 'Pj', 'Gemma', 'James', 'Peter'];
-
-for (member of teachingTeam) {
-    if (member.includes('o')) {
-        console.log(member);
-    }
-}
